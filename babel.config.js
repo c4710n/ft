182cbc1f0bac6093615145aede3e966c9bcce2cd
c4710n <c4710n@users.noreply.github.com>
2018-12-18 17:54:42 +0800

    chore: add misc files

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,21 @@ module.exports = function generateConfig(api) {
     ['@babel/preset-env', { useBuiltIns: 'usage', modules: false }],
   ]
 
+  const plugins = [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathSuffix: './src',
+        rootPathPrefix: '#',
+      },
+    ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-syntax-dynamic-import',
+  ]
+
   return {
     presets,
+    plugins,
   }
 }
