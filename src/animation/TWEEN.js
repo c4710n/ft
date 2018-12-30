aad52210d0f4ba2d9178cd0de785572fff05fcf3
c4710n <c4710n@users.noreply.github.com>
2018-12-30 13:20:34 +0800

    feat: add Tween#loop()

--- src/animation/TWEEN.js
+++ src/animation/TWEEN.js
@@ -1,7 +1,16 @@
 // delegate es6-tween
-import { update } from 'es6-tween'
+import { update, Tween } from 'es6-tween'
 
 const TWEEN = { update }
 
 export default TWEEN
+
+if (!Tween.prototype.loop) {
+  // candy for repeat forever
+  Tween.prototype.loop = function loop() {
+    this.repeat(Number.POSITIVE_INFINITY)
+    return this
+  }
+}
+
 export { Tween, Easing, Interpolation } from 'es6-tween'
