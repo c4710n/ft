5a53516a9a247825f9fdd0cf79273c9dd80858f1
c4710n <c4710n@users.noreply.github.com>
2018-12-31 14:19:41 +0800

    refactor: adjust implementation of Spinner

--- src/display/Spinner.js
+++ src/display/Spinner.js
@@ -1,8 +1,8 @@
 import PIXI from '#/pixi'
 
-const { Sprite, BaseTexture, Texture } = PIXI
+const { Sprite, Texture } = PIXI
 
-const spinner =
+const dataURL =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAA+CAMAAABXyBiCAAAALVBMVEVHcEz////////////////////////////////////////////////////////NXt0CAAAADnRSTlMA6s6hkRr6RTdff3EJvuAfGLoAAAEnSURBVHja7dfbisUgDEBRk2jiNf//uUMhdChWe3IY5qnrsbArRagx3EtSWm2tSAof6hKR9cQjynOUCVRZr4DyNhLUBSzrlVA3UBZLsW6wKqW7DPQJyFz1y7uRiGIccF2R25yxmlGln09zu3xxnLqoB6jTF+QIm64TWzRJtFsvpU82pwSPqAaSq6tqMLgUtq46OzXZ15Fl5Mu6beAIPplt75yKbYGXDKQUXq/X62/kATYG+P98zZuRHjg7M7RDq//LaZB9Z+upftmhfnVIJlATvYePwbx+eV/PDqspLlVQqLsOYr6LDm3RGWy5B9OlDnvMECaN9QJGjESErL84zASuk6POMNzopHssrrn8eXMKLiN5vj6o4/pwkjhYT4xRepisr0atrq9GP5VpI3XLFWQCAAAAAElFTkSuQmCC'
 
 class Spinner extends Sprite {
@@ -10,12 +10,10 @@ class Spinner extends Sprite {
     super()
 
     const image = new Image()
-    image.src = spinner
-    const baseTexture = new BaseTexture(image)
-    const texture = new Texture(baseTexture)
-    this.texture = texture
-    this.tint = color
+    image.src = dataURL
+    this.texture = Texture.from(image)
 
+    this.tint = color
     this.anchor.set(0.5)
     this.scale.set(1.2)
   }
