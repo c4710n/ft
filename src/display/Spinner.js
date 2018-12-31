1f2b8635dc90ba1307403c397fed751f5057c5a6
c4710n <c4710n@users.noreply.github.com>
2018-12-31 14:41:22 +0800

    docs: add docs for Spinner

--- src/display/Spinner.js
+++ src/display/Spinner.js
@@ -1,24 +1,45 @@
 import PIXI from '#/pixi'
-
 const { Sprite, Texture } = PIXI
 
 const dataURL =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAA+CAMAAABXyBiCAAAALVBMVEVHcEz////////////////////////////////////////////////////////NXt0CAAAADnRSTlMA6s6hkRr6RTdff3EJvuAfGLoAAAEnSURBVHja7dfbisUgDEBRk2jiNf//uUMhdChWe3IY5qnrsbArRagx3EtSWm2tSAof6hKR9cQjynOUCVRZr4DyNhLUBSzrlVA3UBZLsW6wKqW7DPQJyFz1y7uRiGIccF2R25yxmlGln09zu3xxnLqoB6jTF+QIm64TWzRJtFsvpU82pwSPqAaSq6tqMLgUtq46OzXZ15Fl5Mu6beAIPplt75yKbYGXDKQUXq/X62/kATYG+P98zZuRHjg7M7RDq//LaZB9Z+upftmhfnVIJlATvYePwbx+eV/PDqspLlVQqLsOYr6LDm3RGWy5B9OlDnvMECaN9QJGjESErL84zASuk6POMNzopHssrrn8eXMKLiN5vj6o4/pwkjhYT4xRepisr0atrq9GP5VpI3XLFWQCAAAAAElFTkSuQmCC'
 
+/**
+ * A simple spinner.
+ *
+ * @example
+ * const spinner = FT.create(Spinner, { color: 0xff0000 })
+ * container.addChild(spinner)
+ */
 class Spinner extends Sprite {
+  /**
+   * @param {Object} options
+   * @param {number} options.color=0x5699d2 spinner's color
+   */
   constructor({ color = 0x5699d2 } = {}) {
     super()
 
     const image = new Image()
     image.src = dataURL
+
+    /**
+     * @ignore
+     */
     this.texture = Texture.from(image)
 
+    /**
+     * @ignore
+     */
     this.tint = color
+
     this.anchor.set(0.5)
     this.scale.set(1.2)
   }
 
   onUpdate() {
+    /**
+     * @ignore
+     */
     this.rotation += 0.1
   }
 }
