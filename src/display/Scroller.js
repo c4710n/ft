99f85f7085c745a7b9de1256a87d281d984abc2f
c4710n <c4710n@users.noreply.github.com>
2019-01-03 15:32:08 +0800

    feat: add more control to display/Scroller

--- src/display/Scroller.js
+++ src/display/Scroller.js
@@ -3,7 +3,15 @@ import TWEEN from '#/animation/TWEEN'
 import PIXI from '#/pixi'
 
 class Scroller extends PIXI.Container {
-  constructor({ width = 1500, height = 750, content, resistance = 20 } = {}) {
+  constructor({
+    width = 1500,
+    height = 750,
+    enableX = true,
+    enableY = false,
+    content,
+    resistance = 20,
+    overflow = 100,
+  } = {}) {
     super()
 
     const { WHITE } = PIXI.Texture
@@ -25,31 +33,34 @@ class Scroller extends PIXI.Container {
     window.on('pointerup', this.onPointerUp)
     window.on('pointerupoutside', this.onPointerUp)
 
+    // scroll
+    this.enableX = enableX
+    this.enableY = enableY
+    this.resistance = resistance
+
+    // position
     this.maxX = 0
     this.minX = width - content.width
     this.maxY = 0
     this.minY = height - content.height
 
-    this.tween = null
-    this.resistance = resistance
-
     // bounce related
-    const overflow = 100
+    this.bounceX = null
+    this.bounceY = null
     this.maxOverflowX = overflow
     this.minOverflowX = this.minX - overflow
     this.maxOverflowY = overflow
     this.minOverflowY = this.minY - overflow
 
     // momentum related
-    this.previousPosition = null
-    this.previousTimestamp = 0
-    this.scrollVelocityX = 0
-    this.scrollVelocityY = 0
+    this.momentumX = null
+    this.momentumY = null
+    this.resetScrollVelocity()
   }
 
   onPointerDown = event => {
     this.isScrolling = true
-    if (this.tween) this.tween.stop()
+    this.stopTween()
 
     this.data = event.data
     this.pointerDownPosition = this.data.getLocalPosition(this.content)
@@ -62,62 +73,102 @@ class Scroller extends PIXI.Container {
 
     const currentPosition = this.data.getLocalPosition(this)
     const currentTimestamp = event.data.originalEvent.timeStamp
-
-    const shiftX = currentPosition.x - this.previousPosition.x
-    const shiftY = currentPosition.y - this.previousPosition.y
     const time = currentTimestamp - this.previousTimestamp
     const factor = 10
-    this.scrollVelocityX = (shiftX / time) * factor
-    this.scrollVelocityY = (shiftY / time) * factor
+
+    if (this.enableX) {
+      const shiftX = currentPosition.x - this.previousPosition.x
+      this.scrollVelocityX = (shiftX / time) * factor
+
+      let x = currentPosition.x - this.pointerDownPosition.x
+      if (x > this.maxOverflowX) {
+        x = this.maxOverflowX
+      } else if (x < this.minOverflowX) {
+        x = this.minOverflowX
+      }
+
+      this.content.x = x
+    }
+
+    if (this.enableY) {
+      const shiftY = currentPosition.y - this.previousPosition.y
+      this.scrollVelocityY = (shiftY / time) * factor
+
+      let y = currentPosition.y - this.pointerDownPosition.y
+      if (y > this.maxOverflowY) {
+        y = this.maxOverflowY
+      } else if (y < this.minOverflowY) {
+        y = this.minOverflowY
+      }
+
+      this.content.y = y
+    }
 
     this.previousPosition = currentPosition
     this.previousTimestamp = currentTimestamp
-
-    let x = currentPosition.x - this.pointerDownPosition.x
-    let y = currentPosition.y - this.pointerDownPosition.y
-    if (x > this.maxOverflowX) {
-      x = this.maxOverflowX
-    } else if (x < this.minOverflowX) {
-      x = this.minOverflowX
-    }
-    if (y > this.maxOverflowY) {
-      y = this.maxOverflowY
-    } else if (y < this.minOverflowY) {
-      y = this.minOverflowY
-    }
-
-    this.content.position.set(x, y)
   }
 
   onPointerUp = () => {
     this.isScrolling = false
+    this.data = null
 
     this.handleBounce()
     this.handleMomentum()
   }
 
   handleBounce = () => {
-    const { content } = this
-    const { x, y } = content
+    const { x, y } = this.content
 
-    const to = {}
+    if (this.enableX && (x > this.maxX || x < this.minX)) {
+      const from = { x }
+      const to = {}
 
-    if (x > this.maxX) {
-      to.x = this.maxX
-    } else if (x < this.minX) {
-      to.x = this.minX
+      if (x > this.maxX) {
+        to.x = this.maxX
+      } else if (x < this.minX) {
+        to.x = this.minX
+      }
+
+      const bounce = new Tween(from)
+        .to(to)
+        .duration(500)
+        .easing(Easing.Quartic.Out)
+        .start()
+      bounce.on('update', ({ x }) => {
+        this.content.x = x
+      })
+
+      this.bounceX = bounce
     }
 
-    if (y > this.maxY) {
-      to.y = this.maxY
-    } else if (y < this.minY) {
-      to.y = this.minY
-    }
+    if (this.enableY && (y > this.maxY || y < this.minY)) {
+      const from = { y }
+      const to = {}
 
-    this.tween = new Tween(content)
-      .to(to, 500)
-      .easing(Easing.Quartic.Out) // this easing is awesome for bounce
-      .start()
+      if (y > this.maxY) {
+        to.y = this.maxY
+      } else if (y < this.minY) {
+        to.y = this.minY
+      }
+
+      const bounce = new Tween(from)
+        .to(to)
+        .duration(500)
+        .easing(Easing.Quartic.Out)
+        .start()
+      bounce.on('update', ({ y }) => {
+        this.content.y = y
+      })
+
+      this.bounceY = bounce
+    }
+  }
+
+  stopTween = () => {
+    if (this.bounceX) this.bounceX.stop()
+    if (this.bounceY) this.bounceY.stop()
+    if (this.momentumX) this.momentumX.stop()
+    if (this.momentumY) this.momentumY.stop()
   }
 
   resetScrollVelocity() {
@@ -128,40 +179,83 @@ class Scroller extends PIXI.Container {
   }
 
   handleMomentum = () => {
-    if (Math.abs(this.scrollVelocityX) < 3) return
+    const absScrollVelocityX = Math.abs(this.scrollVelocityX)
+    const absScrollVelocityY = Math.abs(this.scrollVelocityY)
 
-    let { x: cacheX } = this.content
-    const velocityX = this.scrollVelocityX
+    if (this.enableX && absScrollVelocityX >= 3) {
+      const from = {}
+      const to = {}
+
+      from.velocity = this.scrollVelocityX
+      to.velocity = 0
+
+      let { x: cacheX } = this.content
+      const duration = (100 - this.resistance) * absScrollVelocityX
+
+      const momentum = new Tween(from)
+        .to(to)
+        .duration(duration)
+        .easing(Easing.Quintic.Out)
+        .start()
+
+      momentum.on('update', ({ velocity }) => {
+        cacheX += velocity
+
+        if (cacheX <= this.maxOverflowX && cacheX >= this.minOverflowX) {
+          this.content.x = cacheX
+        } else {
+          /**
+           * This is a workaround for stop tween.
+           * When calling tween.stop(), it will trigger an error.
+           */
+          momentum.pause()
+          TWEEN.remove(momentum)
+          this.handleBounce()
+        }
+      })
+
+      momentum.on('complete', this.handleBounce)
+      this.momentumX = momentum
+    }
+
+    if (this.enableY && absScrollVelocityY >= 3) {
+      const from = {}
+      const to = {}
+
+      from.velocity = this.scrollVelocityY
+      to.velocity = 0
+
+      let { y: cacheY } = this.content
+      const duration = (100 - this.resistance) * absScrollVelocityY
+      this.resetScrollVelocity()
+
+      const momentum = new Tween(from)
+        .to(to)
+        .duration(duration)
+        .easing(Easing.Quintic.Out)
+        .start()
+
+      momentum.on('update', ({ velocity }) => {
+        cacheY += velocity
+
+        if (cacheY <= this.maxOverflowY && cacheY >= this.minOverflowY) {
+          this.content.y = cacheY
+        } else {
+          /**
+           * This is a workaround for stop tween.
+           * When calling tween.stop(), it will trigger an error.
+           */
+          momentum.pause()
+          TWEEN.remove(momentum)
+          this.handleBounce()
+        }
+      })
+
+      momentum.on('complete', this.handleBounce)
+      this.momentumY = momentum
+    }
+
     this.resetScrollVelocity()
-
-    const duration = (100 - this.resistance) * Math.abs(velocityX)
-
-    const tween = new Tween({
-      velocityX,
-    })
-      .to({ velocityX: 0 }, duration)
-      .easing(Easing.Quintic.Out)
-      .start()
-    tween.on('update', ({ velocityX }) => {
-      cacheX += velocityX
-
-      if (cacheX <= this.maxOverflowX && cacheX >= this.minOverflowX) {
-        this.content.x = cacheX
-      } else {
-        /**
-         * This is a workaround for stop tween.
-         * When calling tween.stop(), it will trigger an error.
-         */
-        tween.pause()
-        TWEEN.remove(tween)
-        this.handleBounce()
-      }
-    })
-    tween.on('complete', () => {
-      this.handleBounce()
-    })
-
-    this.tween = tween
   }
 }
 
