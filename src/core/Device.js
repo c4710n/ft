46e19179336735f6ccbb9b5f2924d0a456124021
c4710n <c4710n@users.noreply.github.com>
2019-01-10 14:04:27 +0800

    feat: add device detector

--- src/core/Device.js
+++ src/core/Device.js
@@ -32,6 +32,18 @@ class Device {
   }
 
   /**
+   * User agent.
+   */
+  static get UA() {
+    const ua =
+      window && window.navigator && window.navigator.userAgent
+        ? window.navigator.userAgent
+        : ''
+
+    return ua
+  }
+
+  /**
    * Orientation of current device.
    */
   static get orientation() {
@@ -51,6 +63,37 @@ class Device {
    */
   static get isPortrait() {
     return !Device.isLandscape
+  }
+
+  get isIOS() {
+    const pattern = /ip[honead]{2,4}(?:.*os\s([\w]+)\slike\smac|;\sopera)/i
+    return pattern.test(Device.UA)
+  }
+
+  get isAndroid() {
+    const pattern = /android/i
+    return pattern.test(Device.UA)
+  }
+
+  get isWeChat() {
+    const pattern = /MicroMessenger/i
+    return pattern.test(Device.UA)
+  }
+
+  get isQQ() {
+    const pattern_ios = new RegExp(
+      '(iPad|iPhone|iPod).*? (IPad)?QQ\\/([\\d\\.]+)'
+    )
+    const pattern_android = new RegExp(
+      '\\bV1_AND_SQI?_([\\d\\.]+)(.*? QQ\\/([\\d\\.]+))?',
+      'ig'
+    )
+    return pattern_ios.test(Device.UA) || pattern_android.test(Device.UA)
+  }
+
+  get isQQBrowser() {
+    const pattern = /m?(qqbrowser)[\/\s]?([\w\.]+)/i // eslint-disable-line
+    return pattern.test(Device.UA)
   }
 }
 
