4adfbf01369b0d298d438c4d60f9543a036f450c
c4710n <c4710n@users.noreply.github.com>
2019-01-12 18:38:59 +0800

    feat: add width and height option for utils/capture

--- src/utils/capture.js
+++ src/utils/capture.js
@@ -7,16 +7,22 @@ import PIXI from '#/pixi'
  * @param {Object} [options]
  * @param {string} [options.format='image/png'] the image's format.
  * @param {number} [options.quality=0.92] a number between 0 and 1 indicating the image quality.
+ * @param {number} [options.width] specify another width instead of the width of displayObject
+ * @param {number} [options.height] specify another height instead of the height of displayObject.
  * @return {DataURL} the data url of generated image.
  *
  * @example
  * const dataURL = capture(display, { image: 'image/jpeg', quality: 0.7 })
  */
-function capture(displayObject, { format = 'image/png', quality = 0.92 } = {}) {
-  const { width, height } = displayObject
+function capture(
+  displayObject,
+  { format = 'image/png', quality = 0.92, width, height } = {}
+) {
+  const $width = width !== undefined ? width : displayObject.width
+  const $height = height !== undefined ? height : displayObject.height
 
-  const renderer = PIXI.autoDetectRenderer(width, height)
-  const rt = PIXI.RenderTexture.create(width, height)
+  const renderer = PIXI.autoDetectRenderer($width, $height)
+  const rt = PIXI.RenderTexture.create($width, $height)
   renderer.render(displayObject, rt)
   const dataURL = renderer.extract.canvas(rt).toDataURL(format, quality)
 
