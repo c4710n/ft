e73f82e5d9c8e500e3c84c3e50a9ce51c833af05
c4710n <c4710n@users.noreply.github.com>
2019-01-12 18:22:45 +0800

    feat: add options for utils/capture

--- src/utils/capture.js
+++ src/utils/capture.js
@@ -4,15 +4,21 @@ import PIXI from '#/pixi'
  * Generate DataURL of given display object.
  *
  * @param {DisplayObject} displayObject the display object to be captured.
+ * @param {Object} [options]
+ * @param {string} [options.format='image/png'] the image's format.
+ * @param {number} [options.quality=0.92] a number between 0 and 1 indicating the image quality.
  * @return {DataURL} the data url of generated image.
+ *
+ * @example
+ * const dataURL = capture(display, { image: 'image/jpeg', quality: 0.7 })
  */
-function capture(displayObject) {
+function capture(displayObject, { format = 'image/png', quality = 0.92 } = {}) {
   const { width, height } = displayObject
 
   const renderer = PIXI.autoDetectRenderer(width, height)
   const rt = PIXI.RenderTexture.create(width, height)
   renderer.render(displayObject, rt)
-  const dataURL = renderer.extract.base64(displayObject)
+  const dataURL = renderer.extract.canvas(rt).toDataURL(format, quality)
 
   return dataURL
 }
