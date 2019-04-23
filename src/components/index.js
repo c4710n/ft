31a3834774a5578c1cbdbeff7435d0ed5871d486
c4710n <c4710n@users.noreply.github.com>
2019-04-23 19:25:58 +0800

    cleanup: remove useless parts

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
 export { default as Component } from './Component'
 export { default as Widget } from './Widget'
-export { default as AnimationFade } from './AnimationFade'
-export { default as AnimationBreath } from './AnimationBreath'
 export { default as Draggable } from './Draggable'
-export { default as SpineTimelineController } from './SpineTimelineController'
