3e3345833791d8cd2c9d50fa679345fadfb986d3
c4710n <c4710n@users.noreply.github.com>
2019-04-23 16:24:25 +0800

    feat: set default format of capture to jpeg

--- src/utils/capture.js
+++ src/utils/capture.js
@@ -5,18 +5,18 @@ import PIXI from '#/pixi'
  *
  * @param {DisplayObject} displayObject the display object to be captured.
  * @param {Object} [options]
- * @param {string} [options.format='image/png'] the image's format.
- * @param {number} [options.quality=0.92] a number between 0 and 1 indicating the image quality.
+ * @param {string} [options.format='image/jpeg'] the image's format.
+ * @param {number} [options.quality=0.8] a number between 0 and 1 indicating the image quality.
  * @param {number} [options.width] specify another width instead of the width of displayObject
  * @param {number} [options.height] specify another height instead of the height of displayObject.
  * @return {DataURL} the data url of generated image.
  *
  * @example
- * const dataURL = capture(display, { image: 'image/jpeg', quality: 0.7 })
+ * const dataURL = capture(display, { image: 'image/png', quality: 0.92 })
  */
 function capture(
   displayObject,
-  { format = 'image/png', quality = 0.92, width, height } = {}
+  { format = 'image/jpeg', quality = 0.8, width, height } = {}
 ) {
   const $width = width !== undefined ? width : displayObject.width
   const $height = height !== undefined ? height : displayObject.height
