4fe895196c9ac1388de20b56a784fc20aef8ba34
c4710n <c4710n@users.noreply.github.com>
2019-09-15 17:51:26 +0800

    feat: add more utils for random

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,4 +1,4 @@
-function randomFloat({ min = 0, max, includeMax = false }) {
+export function randomFloat({ min = 0, max, includeMax = false }) {
   if (min === 0) {
     if (includeMax) {
       // [0, max]
@@ -20,7 +20,7 @@ function randomFloat({ min = 0, max, includeMax = false }) {
   }
 }
 
-function randomInteger({ min = 0, max, includeMax = false }) {
+export function randomInteger({ min = 0, max, includeMax = false }) {
   if (min === 0) {
     if (includeMax) {
       // [0, max]
@@ -46,6 +46,51 @@ function random({ min, max, float = false, includeMax = false }) {
   } else {
     return randomInteger({ min, max, includeMax })
   }
+}
+
+export function getRandomElements(array, num) {
+  const total = array.length
+
+  const n = num > total ? total : num
+
+  const shuffledArray = array.sort(() => 0.5 - Math.random())
+  const selected = shuffledArray.slice(0, n)
+  return selected
+}
+
+export function getRandomSeparatedElements(array, num) {
+  const total = array.length
+
+  const n = num > total ? total : num
+
+  const shuffledArray = array.slice().sort(() => 0.5 - Math.random())
+  const selected = shuffledArray.slice(0, n)
+  const rest = shuffledArray.slice(n, total)
+
+  return [selected, rest]
+}
+
+export function getSequentialRandomSeparatedElements(array, num) {
+  const total = array.length
+
+  const n = num > total ? total : num
+
+  const maxSliceEnd = total - n
+  const sliceEnd = randomInteger(maxSliceEnd)
+
+  const selected = array.slice(sliceEnd, sliceEnd + n)
+  const begin = array.slice(0, sliceEnd)
+  const end = array.slice(sliceEnd + n)
+  const rest = [].concat(begin, end)
+
+  return [selected, rest]
+}
+
+export function getRandomElement(array) {
+  const maxIndex = array.length - 1
+  const randomIndex = Math.floor(Math.random() * (maxIndex + 1))
+  const randomOne = array[randomIndex]
+  return randomOne
 }
 
 export default random
