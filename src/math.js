b308ccf3f48e044fbda492327c544243ac5b563a
c4710n <c4710n@users.noreply.github.com>
2019-10-08 14:06:21 +0800

    feat: add math.clamp

---
+++ src/math.js
@@ -0,0 +1,17 @@
+export function clamp(x, min, max) {
+  if (min > max) {
+    throw new RangeError('`min` should be lower than `max`')
+  }
+
+  if (x < min) {
+    return min
+  }
+
+  if (x > max) {
+    return max
+  }
+
+  return x
+}
+
+export default { clamp }
