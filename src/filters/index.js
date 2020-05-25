2bb26f5268655bda01c6d1e7a8991221f31f3abb
c4710n <c4710n@users.noreply.github.com>
2020-05-25 18:24:49 +0800

    feat: re-export all available filters via 'ft/filters'

---
+++ src/filters/index.js
@@ -0,0 +1,11 @@
+import { PIXI } from '../core'
+
+export const AlphaFilter = PIXI.filters.AlphaFilter
+export const BlurFilter = PIXI.filters.BlurFilter
+export const BlurFilterPass = PIXI.filters.BlurFilterPass
+export const ColorMatrixFilter = PIXI.filters.ColorMatrixFilter
+export const DisplacementFilter = PIXI.filters.DisplacementFilter
+export const FXAAFilter = PIXI.filters.FXAAFilter
+export const NoiseFilter = PIXI.filters.NoiseFilter
+
+export default PIXI.filters
